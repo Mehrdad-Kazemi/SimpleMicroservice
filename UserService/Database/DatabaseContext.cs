4ed0aa1eeb2c4a2d8d9592637e51de1f736b8830
Mehrdad Kazemi <Mehrdad.Kazemi@ac-e.be>
2021-10-12 13:42:09 +0200

    Add connection string

---
+++ UserService/Database/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(@"Data Source=localhost;Initial Catalog=UserMicroservices;Integrated Security=True");
+        }
+    }
+}
