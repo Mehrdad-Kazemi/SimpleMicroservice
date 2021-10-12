ef1a07efd4b5b88b33d2ebc30ae626350d5e0e90
Mehrdad Kazemi <Mehrdad.Kazemi@ac-e.be>
2021-10-12 14:35:16 +0200

    Basic CRUD in controller

--- UserService/Controllers/UserController.cs
+++ UserService/Controllers/UserController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using UserService.Database;
+using UserService.Database.Entities;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +15,54 @@ namespace UserService.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        // GET: api/<UserController>
+        DatabaseContext databaseContext;
+        public UserController()
+        {
+            databaseContext = new DatabaseContext();
+        }
+
+        // GET: api/user
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<User> Get()
         {
-            return new string[] { "value1", "value2" };
+            return databaseContext.Users.ToList();
         }
 
-        // GET api/<UserController>/5
+        // GET api/user/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public User Get(int id)
         {
-            return "value";
+            return databaseContext.Users.Find(id);
         }
 
-        // POST api/<UserController>
+        // POST api/user
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] User model)
         {
+            try
+            {
+                databaseContext.Users.Add(model);
+                databaseContext.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, model);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
         }
 
-        // PUT api/<UserController>/5
+        // PUT api/user/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+            // todo
         }
 
-        // DELETE api/<UserController>/5
+        // DELETE api/user/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            // todo
         }
     }
 }
