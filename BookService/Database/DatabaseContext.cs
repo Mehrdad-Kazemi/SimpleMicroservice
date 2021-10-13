67dfd83bdb252193db942f5c01173d05a1695315
Mehrdad Kazemi <Mehrdad.Kazemi@ac-e.be>
2021-10-13 14:51:16 +0200

    Adding BookMicroservice + connection string

---
+++ BookService/Database/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using BookService.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookService.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(@"Data Source=localhost;Initial Catalog=BookMicroservices;Integrated Security=True");
+        }
+    }
+}
---
+++ BookService/Database/Entities/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookService.Database.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
