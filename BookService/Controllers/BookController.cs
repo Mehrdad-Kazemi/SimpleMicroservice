69087cf82778a1709d16fc906fb2c6c58e49ccf3
Mehrdad Kazemi <Mehrdad.Kazemi@ac-e.be>
2021-10-13 15:14:39 +0200

    Add Crud operation to book controller

--- BookService/Controllers/BookController.cs
+++ BookService/Controllers/BookController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookService.Database;
+using BookService.Database.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +15,76 @@ namespace BookService.Controllers
     [ApiController]
     public class BookController : ControllerBase
     {
-        // GET: api/<BookController>
+        DatabaseContext dbContext;
+
+        public BookController()
+        {
+            dbContext = new DatabaseContext();
+        }
+
+        // GET: api/book
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Book> Get()
         {
-            return new string[] { "value1", "value2" };
+            return dbContext.Books.ToList();
         }
 
-        // GET api/<BookController>/5
+        // GET api/book/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Book Get(int id)
         {
-            return "value";
+            return dbContext.Books.FirstOrDefault(x => x.Id == id);
         }
 
-        // POST api/<BookController>
+        // POST api/book
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Book book)
         {
+            try
+            {
+                dbContext.Books.Add(book);
+                dbContext.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, book);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
         }
 
-        // PUT api/<BookController>/5
+        // PUT api/book/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] Book book)
         {
+            try
+            {
+                dbContext.Books.Update(book);
+                dbContext.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, book);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
         }
 
-        // DELETE api/<BookController>/5
+        // DELETE api/book/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            try
+            {
+                var bookToRemove = dbContext.Books.FirstOrDefault(x => x.Id == id);
+                if (bookToRemove == null)
+                    return StatusCode(StatusCodes.Status204NoContent);
+                dbContext.Books.Remove(bookToRemove);
+                dbContext.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, bookToRemove);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
         }
     }
 }
