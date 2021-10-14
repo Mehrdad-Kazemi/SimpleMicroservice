61d3eba323e30b351a03b30b1e97f295745e9cba
Mehrdad Kazemi <Mehrdad.Kazemi@ac-e.be>
2021-10-14 11:41:43 +0200

    Add reservation service

---
+++ ReservationService/Database/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReservationService.Database.Entities;
+
+namespace ReservationService.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Reservation> Reservations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(@"Data Source=localhost;Initial Catalog=ReservationMicroservices;Integrated Security=True");
+        }
+    }
+}
---
+++ ReservationService/Database/Entities/Reservation.cs
@@ -0,0 +1,9 @@
+﻿namespace ReservationService.Database.Entities
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+    }
+}
